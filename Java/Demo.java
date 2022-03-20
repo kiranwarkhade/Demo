0bd0f4b041181adcf4517a88d1a70bb085cf5916
apex/kiran warkhade <kiranwarkhade1999@gmail.com>
2022-03-20 10:52:00 +0000

    tenth commit

--- Java/Demo.java
+++ Java/Demo.java
@@ -2,6 +2,26 @@ public class Demo {
     public static void main(String[] args) {
         
         System.out.println("Hello Java program ");
+
+        String s="              Balaji   Kumar   ";
+        String s1="              Balaji   Kumar   ";
+
+
+        String ss="Kiran Kumar";
+
+
+        System.out.println(s==s1);
+        System.out.println(s.equals("              Balaji   Kumar   "));
+
+
+
+        System.out.println(s);
+        System.out.println(s.strip());
+        System.out.println(s.concat("Hari"));
+        System.out.println(s.replaceAll(s, "jayram"));
+        System.out.println(s.toCharArray());
+       System.out.println( s.split(s));
+       
     }
 
 }
