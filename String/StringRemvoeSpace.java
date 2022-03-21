2c9b78c9761d44b91684c4d295f3ecb2b44a91a5
apex/kiran warkhade <kiranwarkhade1999@gmail.com>
2022-03-21 11:02:15 +0000

    fourth commit

---
+++ String/StringRemvoeSpace.java
@@ -0,0 +1,17 @@
+
+class StringRemvoeSpace
+{
+
+public static void main(String[] args) {
+    
+
+
+System.out.println(" D ev  Dum my ".replace(" ",""));
+
+System.out.println(" D ev  Dum my ".replaceAll(" ",""));
+
+System.out.println(" D ev  Dum my ".replaceAll("\\s+",""));
+
+
+}
+}
